ef3c1c3ac6da7f59b46f60267f38a64c5f98837d
kimchanmin <rlacksals96@gmail.com>
2021-03-25 01:27:54 +0900

    init: splash, login page..done, home..working

---
+++ app/src/main/java/com/example/evsherpa/LoadingActivity.java
@@ -0,0 +1,32 @@
+package com.example.evsherpa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.evsherpa.ui.login.LoginActivity;
+
+
+public class LoadingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loading);
+
+        startLoading();
+    }
+
+    private void startLoading(){
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+    }
+}
