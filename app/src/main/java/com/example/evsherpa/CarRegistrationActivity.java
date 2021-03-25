5e9eab386e004606fd0b79c94fc9378efadf4530
김찬민 <rlacksals96@gmail.com>
2021-03-25 17:38:55 +0900

    add:preference rank page..need to fix->chk commit note
    이미 카운트는 3이 넘었는데도 if-else문 반영 안되서 클릭이 활성화되는 문제가 있음.

--- app/src/main/java/com/example/evsherpa/CarRegistrationActivity.java
+++ app/src/main/java/com/example/evsherpa/CarRegistrationActivity.java
@@ -2,7 +2,10 @@ package com.example.evsherpa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CarRegistrationActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class CarRegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_registration);
+
+        Button btn_car_registration_submit=findViewById(R.id.btn_car_registration_submit);
+        btn_car_registration_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(CarRegistrationActivity.this,PreferenceRegistrationActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
