45f7d0a9c493ec00c543bb8aea0f5492adc25ef5
kimchanmin <rlacksals96@gmail.com>
2021-05-10 19:31:56 +0900

    add:outlet recycler view..hotfix:see todo_list in log content"
    todo:
    	add outlet data json file in application
    	-> make CarOultletData object so that null point execption is not called

--- app/src/main/java/com/example/evsherpa/ui/carOutlet/CarOutletFragment.java
+++ app/src/main/java/com/example/evsherpa/ui/carOutlet/CarOutletFragment.java
@@ -11,25 +11,51 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.evsherpa.R;
 
+import java.util.ArrayList;
+
 public class CarOutletFragment extends Fragment {
 
-    private CarOutletViewModel carOutletViewModel;
+    private ArrayList<CarOutletData> arrayList;
+    private CarOutletAdapter carOutletAdapter;
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        carOutletViewModel =
-//                new ViewModelProvider(this).get(CarOutletViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_car_outlet, container, false);
-//        final TextView textView = root.findViewById(R.id.text_car_outlet);
-//        carOutletViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-//            @Override
-//            public void onChanged(@Nullable String s) {
-//                textView.setText(s);
-//            }
-//        });
-        return root;
+
+        View view = inflater.inflate(R.layout.fragment_car_outlet, container, false);
+
+        recyclerView=(RecyclerView)view.findViewById(R.id.outlet_rv);
+
+        linearLayoutManager=new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        arrayList=new ArrayList<>();
+        carOutletAdapter=new CarOutletAdapter(arrayList);
+        recyclerView.setAdapter(carOutletAdapter);
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        initDataset();
+    }
+    private void initDataset(){
+        //for testing
+
     }
 }
