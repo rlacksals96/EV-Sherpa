6116c08bcfe06ed775c17be0231cc29401597714
kimchanmin <rlacksals96@gmail.com>
2021-05-24 18:33:19 +0900

    add:프로필 서버에 post하는 기능 추가, except address

--- app/src/main/java/com/example/evsherpa/Address_registration.java
+++ app/src/main/java/com/example/evsherpa/Address_registration.java
@@ -3,51 +3,56 @@ package com.example.evsherpa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.TextView;
 
 public class Address_registration extends AppCompatActivity {
 
-    private WebView addressPage;
+    private WebView browser;
     private TextView txt_addr;
     private Handler handler;
+
+    class MyJavaScriptInterface{
+
+        @JavascriptInterface
+        @SuppressWarnings("unused")
+        public void processData(String data){
+            Bundle extra=new Bundle();
+            Intent intent=new Intent();
+            extra.putString("data",data);
+            intent.putExtras(extra);
+            setResult(RESULT_OK,intent);
+            finish();
+        }
+    }
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_address_registration);
 
-        txt_addr=findViewById(R.id.txt_address_result);
-        init_webView();
+        browser = (WebView) findViewById(R.id.webview_address);
+        browser.getSettings().setJavaScriptEnabled(true);
+        browser.addJavascriptInterface(new MyJavaScriptInterface(), "Android");
 
-        handler=new Handler();
+        browser.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                browser.loadUrl("javascript:sample2_execDaumPostcode();");
+            }
+        });
+
+        browser.loadUrl("http://a4382432.dothome.co.kr/daum.html");
     }
 
-    @SuppressLint("SetJavaScriptEnabled")
-    void init_webView(){
-        addressPage=findViewById(R.id.webview_address);
-        addressPage.getSettings().setJavaScriptEnabled(true);
-        addressPage.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-        addressPage.addJavascriptInterface(new AndroidBridge(),"TestApp");
-        addressPage.setWebChromeClient(new WebChromeClient());
 
-        //addressPage.loadUrl("localhost:5500/address.html"); //address창 띄워줄 서버만 잇으면 된다.
-    }
 
-    private class AndroidBridge{
-        @JavascriptInterface
-        public void setAddress(final String arg1,final String arg2,final String arg3){
-            handler.post(new Runnable() {
-                @Override
-                public void run() {
-                    txt_addr.setText(String.format("(%s) %s %s",arg1,arg2,arg3));
 
-                    init_webView();
-                }
-            });
-        }
-    }
+
 }
